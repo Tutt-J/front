92863f1eef35e46ae2a90356a85b93365341b263
Wy <wy@mail.com>
2024-05-23 20:20:24 +0200

    add env for api

--- src/App.jsx
+++ src/App.jsx
@@ -6,7 +6,7 @@ function App() {
 
   useEffect(() => {
     // Faire une requête GET vers le serveur Node.js
-    axios.get('http://localhost:3000/api')
+    axios.get(process.env.API_URL+'/api')
       .then(response => {
         setMessage(response.data);
       })
